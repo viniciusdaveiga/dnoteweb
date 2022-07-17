da1449b9f1964e84d7fcd89e4c3133e2deceef5d
Vinícius André da Veiga <viniciusdaveiga.dev@gmail.com>
2022-07-17 15:00:37 -0300

    Improve the interface style

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ const body = document.querySelector('body'),
       closeToggle = body.querySelector(".close-sidebar"),
       openToggle = body.querySelector(".open-sidebar"),
       teste = body.querySelector(".color-e-title"),
-      teste2 = body.querySelector('.note-area');
+      teste2 = body.querySelector('.note-area'),
+      teste3 = body.querySelector('.toggle-toolbar'),
+      toolbar = body.querySelector('.top-toolbar');
 
 
 closeToggle.addEventListener("click" , () =>{
@@ -18,6 +20,10 @@ openToggle.addEventListener("click" , () =>{
     teste2.classList.toggle("left");
 })
 
+teste3.addEventListener("click" , () =>{
+    toolbar.classList.toggle("toolbar-closed");
+})
+
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
@@ -25,21 +31,41 @@ function myFunction() {
 }
 
 function myFunction2() {
-    document.getElementById("dropdown-alignText").classList.toggle("show-dropdown-alignText");
+    document.querySelector(".content-dropdown-alignText").classList.toggle("show-dropdown-alignText");
+}
+
+function myFunction3() {
+    document.querySelector(".content-dropdown-colorFilter--sidebar").classList.toggle("show-dropdown-colorFilter");
+}
+
+function myFunction4() {
+    document.querySelector(".content-dropdown-colorFilter--top-bar").classList.toggle("show-dropdown-colorFilter");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
-    if (!e.target.matches('.alignText')) {
-        var myDropdown = document.getElementById("dropdown-alignText");
-        if (myDropdown.classList.contains('show-dropdown-alignText')) {
-            myDropdown.classList.remove('show-dropdown-alignText');
-        }
-    }
     if (!e.target.matches('#fontSize')) {
         var myDropdown = document.querySelector(".content-dropdown-fontSize");
         if (myDropdown.classList.contains('show-dropdown-fontSize')) {
             myDropdown.classList.remove('show-dropdown-fontSize');
+        }
+    }
+    if (!e.target.matches('.alignText')) {
+        var myDropdown = document.querySelector(".content-dropdown-alignText");
+        if (myDropdown.classList.contains('show-dropdown-alignText')) {
+            myDropdown.classList.remove('show-dropdown-alignText');
+        }
+    }
+    if (!e.target.matches('#color-filter-sidebar .icon, #color-filter-sidebar .circle')) {
+        var myDropdown = document.querySelector(".content-dropdown-colorFilter--sidebar");
+        if (myDropdown.classList.contains('show-dropdown-colorFilter')) {
+            myDropdown.classList.remove('show-dropdown-colorFilter');
+        }
+    }
+    if (!e.target.matches('#color-filter-topbar .icon, #color-filter-topbar .circle')) {
+        var myDropdown = document.querySelector(".content-dropdown-colorFilter--top-bar");
+        if (myDropdown.classList.contains('show-dropdown-colorFilter')) {
+            myDropdown.classList.remove('show-dropdown-colorFilter');
         }
     }
 }
