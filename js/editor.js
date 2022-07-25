efb216adb3edf51a27c1b0aab2bfedd428006814
Vinícius André da Veiga <viniciusdaveiga.dev@gmail.com>
2022-07-25 15:35:06 -0300

    Change features on the toolbar

--- js/editor.js
+++ js/editor.js
@@ -3,6 +3,37 @@ const editor = document.querySelector('.editable-box');
 function actionCommand(command) {
   document.execCommand(command, false, null);
 }
+function actionComArg(command, arg) {
+  document.execCommand(command, false, arg);
+}
+
+const inputFile = document.getElementById('imageUpload');
+inputFile.onchange = function () {
+  let url = null;
+  let fileObj = document.getElementById("imageUpload").files[0];
+  if (window.createObjcectURL != undefined) {
+    url = window.createOjcectURL(fileObj);
+  } else if (window.URL != undefined) {
+    url = window.URL.createObjectURL(fileObj);
+  } else if (window.webkitURL != undefined) {
+    url = window.webkitURL.createObjectURL(fileObj);
+  }
+  console.log(url)
+  actionComArg('insertImage',url)
+}
+var showingSouceCode = false;
+function toggleCode(buttonId) {
+  if (buttonId) {
+    buttonClicked(buttonId);
+  }
+  if (showingSouceCode) {
+    editor.innerHTML = editor.textContent;
+    showingSouceCode = false;
+  } else {
+    editor.textContent = editor.innerHTML;
+    showingSouceCode = true;
+  }
+}
 
 function blockquote() {
   const element = document.createElement("blockquote");
@@ -31,13 +62,17 @@ document.onselectionchange = () => {
   const testAll = [btnBold, btnItalic, btnUnder, btnStrike, btnSup, btnSub, btnQuote]
   const tags = ['B', 'I', 'U', 'STRIKE', 'SUP', 'SUB', 'BLOCKQUOTE']
 	if (editor === document.activeElement){
-    testAll.forEach((itemBtn, indexBtn)=>{
-      if (isSelectionInTag(tags[indexBtn])){
-        itemBtn.classList.add('active');
+    let i = 0
+    let n = 0
+    while (testAll[i]){
+      if (isSelectionInTag(tags[n])){
+        testAll[i].classList.add('active');
       } else {
-        itemBtn.classList.remove('active');
+        testAll[i].classList.remove('active');
       }
-    })
+      i++;
+      n++;
+    }
 	}
 }
 
